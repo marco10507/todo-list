a035852c1c2610b19de2631dfc552962b3b55228
marco10507 <marco10507@hotmail.com>
2021-02-28 12:21:59 +0100

    using moment to use today and tomorrow

--- src/TasksList.js
+++ src/TasksList.js
@@ -3,58 +3,13 @@ import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { BsCheck, BsX } from "react-icons/bs";
+import moment from "moment";
 
 export default function TaskList(props) {
   const filteredTasks = props.tasks.filter((task) => !task.completed);
   const tasks = filteredTasks.sort(
     (task1, task2) => task1.dueDate - task2.dueDate
   );
-
-  function getToday() {
-    const t = new Date();
-    t.setHours(0, 0, 0, 0);
-
-    return t;
-  }
-
-  function getTomorrow() {
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    tomorrow.setHours(0, 0, 0, 0);
-
-    return tomorrow;
-  }
-
-  function getYesterday() {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    yesterday.setHours(0, 0, 0, 0);
-
-    return yesterday;
-  }
-
-  function getLocaleDateString(date) {
-    const today = getToday().getTime();
-    const tomorrow = getTomorrow().getTime();
-    const yesterday = getYesterday().getTime();
-
-    let dateString = "";
-    switch (date.setHours(0, 0, 0, 0)) {
-      case today:
-        dateString = "Today";
-        break;
-      case tomorrow:
-        dateString = "Tomorrow";
-        break;
-      case yesterday:
-        dateString = "Yesterday";
-        break;
-      default:
-        dateString = date.toLocaleDateString();
-    }
-
-    return dateString;
-  }
 
   function createActionsGroup(task) {
     return (
@@ -73,11 +28,22 @@ export default function TaskList(props) {
     );
   }
 
+  function createDate(date) {
+    return moment(date).calendar({
+      sameDay: "[Today]",
+      nextDay: "[Tomorrow]",
+      nextWeek: "DD/MM/YYYY",
+      lastDay: "[Yesterday]",
+      lastWeek: "DD/MM/YYYY",
+      sameElse: "DD/MM/YYYY"
+    });
+  }
+
   function createRow(task) {
     return (
       <tr key={task.id}>
         <td>{task.subject}</td>
-        <td>{getLocaleDateString(task.dueDate)}</td>
+        <td>{createDate(task.dueDate)}</td>
         <td>{createActionsGroup(task)}</td>
       </tr>
     );
