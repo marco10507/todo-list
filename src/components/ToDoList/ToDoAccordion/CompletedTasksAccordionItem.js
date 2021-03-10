7dcc2887e44bd412d3b26d13cd84e678f8b53737
Marco Fernandez Burgos <marco.burgos@expereo.com>
2021-03-10 17:56:08 +0100

    refactored CompletedTasksAccordionItem

--- src/components/ToDoList/ToDoAccordion/CompletedTasksAccordionItem.js
+++ src/components/ToDoList/ToDoAccordion/CompletedTasksAccordionItem.js
@@ -4,17 +4,17 @@ import CompletedTaskList from "../CompletedTaskList";
 import React from "react"
 
 export default function CompletedTasksAccordionItem(props) {
-  const { tasks, eventKey } = props;
-  return (
-    <>
-      <Accordion.Toggle className="text-center" as={Card.Header} eventKey="1">
-        Completed Tasks
-      </Accordion.Toggle>
-      <Accordion.Collapse eventKey={eventKey}>
-        <Card.Body>
-          <CompletedTaskList tasks={tasks} />
-        </Card.Body>
-      </Accordion.Collapse>
-    </>
-  );
+    const {tasks, eventKey} = props;
+    return (
+        <>
+            <Accordion.Toggle className="text-center" as={Card.Header} eventKey="1">
+                Completed Tasks
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={eventKey}>
+                <Card.Body>
+                    <CompletedTaskList tasks={tasks}/>
+                </Card.Body>
+            </Accordion.Collapse>
+        </>
+    );
 }
