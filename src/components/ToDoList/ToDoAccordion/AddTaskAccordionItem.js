3278b59d28a17cd7c30be13e81ce9d6d011f73c0
Marco Fernandez Burgos <marco.burgos@expereo.com>
2021-03-10 17:54:52 +0100

    refactored AddTaskAccordionItem

--- src/components/ToDoList/ToDoAccordion/AddTaskAccordionItem.js
+++ src/components/ToDoList/ToDoAccordion/AddTaskAccordionItem.js
@@ -4,25 +4,25 @@ import Card from "react-bootstrap/Card";
 import React from "react"
 
 export default function AddTaskAccordionItem(props) {
-  const { task, handleTaskOnChange, handleCreateTask, eventKey } = props;
-  return (
-    <>
-      <Accordion.Toggle
-        className="text-center"
-        as={Card.Header}
-        eventKey={eventKey}
-      >
-        Add Task
-      </Accordion.Toggle>
-      <Accordion.Collapse eventKey={eventKey}>
-        <Card.Body>
-          <AddTask
-            task={task}
-            handleTaskOnChange={handleTaskOnChange}
-            handleCreateTask={handleCreateTask}
-          />
-        </Card.Body>
-      </Accordion.Collapse>
-    </>
-  );
+    const {task, handleTaskOnChange, handleCreateTask, eventKey} = props;
+    return (
+        <>
+            <Accordion.Toggle
+                className="text-center"
+                as={Card.Header}
+                eventKey={eventKey}
+            >
+                Add Task
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={eventKey}>
+                <Card.Body>
+                    <AddTask
+                        task={task}
+                        handleTaskOnChange={handleTaskOnChange}
+                        handleCreateTask={handleCreateTask}
+                    />
+                </Card.Body>
+            </Accordion.Collapse>
+        </>
+    );
 }
